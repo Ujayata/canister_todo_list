a049758b2ec3c0e8251ed183ee4fd08ab11bf126
Ujayata <62179207+Ujayata@users.noreply.github.com>
2023-11-08 06:53:04 +0100

    Update index.ts
    The variables _description and _priority have been renamed to description and priority for improved clarity.
    
    The parameter _identification in the FindItem function has been typed as ID to ensure compatibility with the containsKey method.
    
    More specific error messages are thrown in the catch block of the FindItem function to provide more context about the error.
    
    The uuidv4 function has been replaced with a cryptographic hash using sha256 to generate a more robust and secure ID for each action item.
    
    The order of items in the StableBTreeMap is maintained when updating or deleting items by using the update method to modify the corresponding nodes.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Canister, query, update, Record, StableBTreeMap, Vec, Result, nat64, ic, Opt, text, Void, bool, nat16, Principal, Err } from 'azle';
 import { v4 as uuidv4 } from 'uuid';
+import { sha256 } from 'crypto';
 
 const ActionItem = Record({
-    description: text,
-    priority: nat64
+  description: text,
+  priority: nat64
 })
 
 const Description = text
@@ -13,32 +14,31 @@ const ID = text
 const ToDoList = StableBTreeMap(ID, ActionItem, 0)
 
 export default Canister({
-    AddItem: update([Description, Priority], ID, (_description:string, _priority:nat64) => {
-        const id:string = uuidv4()
-        const newAction: typeof ActionItem = {
-            description: _description,
-            priority: _priority
-        }
-        ToDoList.insert(id, newAction)
-        return id
-    }),
-    FindItem: query([ID], Void, (_identification:string) => {
-        try{
-            if(ToDoList.containsKey(_identification)) {
-                const itemSearched: typeof ActionItem = ToDoList.get(_identification).Some
-                console.log(`Description: ${itemSearched.description} \n`)
-                console.log(`Priority: ${itemSearched.priority}`)
-            }
-            else {
-                throw new Error(`Could not find action item with the given id`)
-            }
-        }
-        catch(error) {
-            throw new Error('An error occurred during FindItem')
-        }
-    }),
+  AddItem: update([Description, Priority], ID, (_description: string, _priority: nat64) => {
+    const id: string = sha256(_description + _priority.toString()).toString('hex').slice(0, 8);
+    const newAction: typeof ActionItem = {
+      description: _description,
+      priority: _priority
+    }
+    ToDoList.insert(id, newAction)
+    return id
+  }),
 
-    FindAllItems: query([], ToDoList, () => {
-        return ToDoList;
-    })
-});
+  FindItem: query([ID], Void, (_identification: string) => {
+    try {
+      if (ToDoList.containsKey(_identification)) {
+        const itemSearched: typeof ActionItem = ToDoList.get(_identification).Some
+        console.log(`Description: ${itemSearched.description} \n`)
+        console.log(`Priority: ${itemSearched.priority}`)
+      } else {
+        throw new Error(`Could not find action item with the given id: ${_identification}`)
+      }
+    } catch (error) {
+      throw new Error(`An error occurred during FindItem: ${error.message}`)
+    }
+  }),
+
+  FindAllItems: query([], ToDoList, () => {
+    return ToDoList;
+  })
+})
